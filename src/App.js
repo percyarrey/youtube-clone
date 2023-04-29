540f9097d1048906d188592ecf6b233a15f4dd32
PERCY <tanyitikuarrey@gmail.com>
2023-04-29 15:11:12 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fa500Px } from "react-icons/fa";
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
